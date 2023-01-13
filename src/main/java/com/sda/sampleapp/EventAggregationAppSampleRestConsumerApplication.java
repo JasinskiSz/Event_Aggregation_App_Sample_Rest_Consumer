d2e53cc5d080893dc334640ff84bddf8b06d6896
Rabioxxx <szg.jasinski@gmail.com>
2023-01-13 16:37:29 +0100

    Initial commit

---
+++ src/main/java/com/sda/sampleapp/EventAggregationAppSampleRestConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.sda.sampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventAggregationAppSampleRestConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventAggregationAppSampleRestConsumerApplication.class, args);
+    }
+}
