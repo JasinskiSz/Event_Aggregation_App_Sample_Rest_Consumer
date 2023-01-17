35a6218c7e2eef3bc34e8a2578998dde43ce42ff
Rabioxxx <szg.jasinski@gmail.com>
2023-01-17 15:53:40 +0100

    Changed name to match the class returned by the API call. It must be the same. Image is now a URL to it. Renamed to AppConfig
    Signed-off-by: Rabioxxx <szg.jasinski@gmail.com>

--- src/main/java/com/sda/sampleapp/model/EventApi.java
+++ src/main/java/com/sda/sampleapp/model/EventApi.java
@@ -6,17 +6,17 @@ import java.time.LocalDateTime;
 import java.util.Set;
 
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
-public class Event {
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventApi {
     private Long id;
     private String title;
     private String description;
     private LocalDateTime startingDateTime;
     private LocalDateTime endingDateTime;
-    private Image image;
+    private String imageUrl;
     private Set<String> usersNicknames;
     private String ownerNickname;
 }
